0cc0c7d3970662fbb9827075a30012a304eb85c9
a820922716 <49236065+a820922716@users.noreply.github.com>
2019-09-19 20:02:02 +0800

    Add files via upload

---
+++ pages/subPages/about-page/about-page.js
@@ -0,0 +1,26 @@
+Page({
+  data:{
+    info:[
+      {
+        label:'姓名',
+        value:'邹攀'
+      },
+      {
+        label: '职业',
+        value: '学生'
+      },
+      {
+        label: 'QQ',
+        value: '820922716'
+      },
+      {
+        label: '微信',
+        value: 'a820922716'
+      },
+      {
+        label: 'github',
+        value: 'https://github.com/a820922716'
+      }
+    ]
+  }
+})
---
+++ pages/tabBar/user/user.js
@@ -0,0 +1,8 @@
+Page({
+  onShareAppMessage(res){
+    return {
+      title:'清风影院',
+      path:'pages/tabBar/movie/movie'
+    }
+  }
+})
